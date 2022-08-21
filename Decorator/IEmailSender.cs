5dccd9f3871a1ed26944a8e805ba2719913d5dfc
a m <clasicnew1@gmail.com>
2022-08-21 23:10:52 +0430

    Add project files.

---
+++ Decorator/IEmailSender.cs
@@ -0,0 +1,27 @@
+﻿namespace Decorator;
+
+public interface IEmailSender
+{
+   void Send();
+}
+public class EmailSender:IEmailSender{
+    public void Send()
+    {
+        Console.WriteLine("Send Email");
+    }
+}
+
+public class DecorateIEmailSender : IEmailSender
+{
+    private IEmailSender EmailSender { get; set;}
+        public DecorateIEmailSender(IEmailSender emailSender)
+        {
+            EmailSender = emailSender; 
+        }
+
+        public void Send()
+        {
+            EmailSender.Send();
+            Console.WriteLine("Decorate Email Sender");
+        }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Decorator;
+
+test test = new test();  
+  EmailSender emailSender = new EmailSender();
+  test.action(emailSender);
+ConcereteComponent concereteComponent = new ConcereteComponent();
+ConcereteDecorator concerete = new(concereteComponent);
+concerete.print();
+public  class test
+{
+   public virtual void action(IEmailSender emailSender)
+    {
+        emailSender.Send();
+        Console.WriteLine("\n------------------------------------------------------\n");
+        DecorateIEmailSender decorateIEmailSender = new DecorateIEmailSender(emailSender);
+        decorateIEmailSender.Send();
+    }
+
+}
+
+public abstract class Component
+{
+    public virtual void print(){}
+}
+
+public class ConcereteComponent : Component
+{
+    public override void print()
+    {
+        Console.WriteLine("Concerete Component");
+    }
+}
+
+public abstract class DecoratorC  : Component
+{
+    public Component Component;
+    public DecoratorC(Component component)
+    {
+        Component = component;
+    }
+    public override void print()
+    {
+        Component.print();
+        Console.WriteLine("Decorate CComponent");
+    }
+    public abstract void NewFeature();
+}
+
+public class ConcereteDecorator : DecoratorC
+{
+    private Component component;
+    public ConcereteDecorator(Component component) :base(component)
+    {
+        
+    }
+    public override void print()
+    {
+        base.Component.print();
+        Console.WriteLine("ConcereteDecorator Component");
+        NewFeature(); 
+    }
+    public override void NewFeature()
+    {
+
+        Console.WriteLine("new f");
+    }
+}
